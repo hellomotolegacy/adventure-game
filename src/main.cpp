24a2e32c0b0f74aaf37cabf02666a8ed48fee233
Ian <hellomotolegacy@gmail.com>
2018-02-09 01:34:03 -0500

    Proper exiting, looping, and key presses

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,31 @@
 
 #include <stdio.h>
 #include <SDL2/SDL.h>
-int main()
+
+SDL_Window *window;
+void CreateWindow()
 {
-	SDL_Window *window;
-	SDL_Init(SDL_INIT_VIDEO);
-	window = SDL_CreateWindow("An SDL Window..", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
+	window = SDL_CreateWindow("Adventure", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
 	if (window == NULL) {
 		printf("Window failed to be created.. (%s)", SDL_GetError());
-		return 1;
+		SDL_Quit();
 	}
-	SDL_Delay(3000);
+}
+int main()
+{
+	bool quit = false;
+	SDL_Init(SDL_INIT_VIDEO);
+	CreateWindow();
+	while (!quit) {
+		const Uint8 *keys = SDL_GetKeyboardState(NULL);
+		SDL_Event mainEvent;
+		while (SDL_PollEvent(&mainEvent)) {
+			if (mainEvent.type == SDL_QUIT)
+				quit = true;
+			if (keys[SDL_SCANCODE_A])
+				printf("move left pls kthx :D\n");
+		}
+	}
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
